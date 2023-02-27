6c99004c58e39b67175b3bea959b731feced5ef7
Xavier Roig Martin <xroigmartin@gmail.com>
2023-02-27 20:17:30 +0100

    create msc_domain project

---
+++ msc_domain/src/test/java/xroigmartin/msc/domain/MscDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.msc.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
